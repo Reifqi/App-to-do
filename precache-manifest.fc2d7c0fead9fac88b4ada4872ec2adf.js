ecd7eb44a42521e08c58d3be164d750f1c75edb1
Reifqi <rifqi_m20@mhs.unsyiah.ac.id>
2022-06-21 13:27:51 +0700

    Updates

---
+++ precache-manifest.fc2d7c0fead9fac88b4ada4872ec2adf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0da36713fa97c029c2e",
+    "url": "/App-to-do/static/css/main.8a70a871.chunk.css"
+  },
+  {
+    "revision": "e0da36713fa97c029c2e",
+    "url": "/App-to-do/static/js/main.e0da3671.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/App-to-do/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "b927bed117894ca6e913",
+    "url": "/App-to-do/static/js/2.b927bed1.chunk.js"
+  },
+  {
+    "revision": "50da872db0957c341b7e",
+    "url": "/App-to-do/static/js/runtime~main.50da872d.js"
+  },
+  {
+    "revision": "85074bfe109b1aa5043c161540390c9c",
+    "url": "/App-to-do/index.html"
+  }
+];
